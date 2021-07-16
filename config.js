72bb9cec0da5ebb16a4b428a4294dcad8deea5c0
Timo_Botz <84431588+Tech-404@users.noreply.github.com>
2021-07-16 08:46:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 global.owner = ['6281515860089', '6281334177340'] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.mods = ['6281515860089','6281334177340','6281261470107'] // Want some help?
+global.prems = ['6281515860089','6281334177340','6281261470107'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
